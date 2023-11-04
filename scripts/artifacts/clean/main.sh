5c0135bc5bf844e3166ade64865d951c5ed1af2b
Italo Alfaro <59491697+Akuqt@users.noreply.github.com>
2023-11-04 10:24:11 -0500

    feat: create artifacts cleaner

---
+++ scripts/artifacts/clean/main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Refresh info
+rm -rf artifacts/info.json
+cp -r info.json artifacts/info.json
+# Refresh tarballs
+rm -rf artifacts/tarballs
+cp -r tarballs artifacts/tarballs
+# Move to artifacts
+cd artifacts
